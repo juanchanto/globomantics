a9e3f973d8cacd9484e4a8d3752c37ffd41042bf
juanchanto <juanchanto96@gmail.com>
2023-12-19 17:22:55 -0600

    Adding New Components

---
+++ components/banner.js
@@ -0,0 +1,13 @@
+const Banner = () => {
+    return(
+        <header>
+            <div>
+                <img src="./GloboLogo.png" alt="logo" />
+            </div>
+            <div>
+                Providing houses all over the world
+            </div>
+        </header>
+    );
+};
+export default Banner;
